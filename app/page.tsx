7949dc3d8f9991fafbc6ae9ed06a810c0fa7634b
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-02-14 12:49:30 +0700

    update background

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,26 @@ import Contact from "@/sections/Contact-Section";
 import FloatingLines from "@/components/bits/FloatingLine";
 import Footer from "@/components/layouts/Footer";
 import { motion } from "framer-motion";
+import LightRays from "@/components/LightRays";
 
 export default function Home() {
   return (
     <PortfolioLayout>
-      <div className="fixed inset-0 -z-10 pointer-events-none">
-        <FloatingLines
-          linesGradient={['#8b5cf6', '#3b82f6', '#06b6d4']}
-          animationSpeed={1}
-          interactive={true}
-          parallax={true}
-          mixBlendMode="normal"
+      <div className="fixed inset-0 -z-10 pointer-events-none bg-black">
+        <LightRays
+            raysOrigin="top-center"
+            raysColor="#dc06f9"
+            raysSpeed={1}
+            lightSpread={0.8}
+            rayLength={8}
+            followMouse={true}
+            mouseInfluence={0.4}
+            noiseAmount={0.5}
+            distortion={0}
+            className="custom-rays"
+            pulsating={false}
+            fadeDistance={1}
+            saturation={1}
         />
       </div>
       <HeroSection />
